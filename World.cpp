63a48f4ad506d675f7d0ae82ea113ea961fdf049
reu <reu@iwanaga-no-MacBook-Air.local>
2016-03-18 18:35:02 +0900

    neibor for hex : need testing

--- World.cpp
+++ World.cpp
@@ -103,6 +103,153 @@ LABEL_FOUND_COAST:
 	return lst;
 }
 
+std::list<int> makeListNeighborHexes(int index, int height, int width, bool isCyinder)
+{
+	/*  width = 3
+	 *
+	 *     0 1 2
+	 *	 +-------
+	 * 0 | 0 1 2
+	 * 1 |  3 4 5
+	 * 2 | 6 7 8
+	 * 3 |  9 a b 
+	 */
+	int y = width / index;
+	int x = width % index; 
+	std::list<int> lst;
+
+	//// left ////
+
+	// 3 -> 5 
+	if (x == 0 && isCyinder) {
+		lst.push_back(index + width - 1);	
+	}
+	// 3 
+	else if (x == 0 && ! isCyinder) {
+		// pass
+	}
+	// 4 -> 3
+	else {
+		lst.push_back(index - 1);	
+	}
+
+
+	//// right ////
+
+	// 5 -> 3
+	if (x == width - 1 && isCyinder) {
+		lst.push_back(index - width + 1);	
+	}
+	// 5
+	else if (x == width - 1 && ! isCyinder) {
+		// pass
+	}
+	// 4 -> 5
+	else {
+		lst.push_back(index + 1);	
+	}
+
+
+	if (y > 0) {
+		// y == 0 or 2
+		if (y % 2 == 0) {
+
+			//// upper left ////
+
+			// 6 -> 5
+			if (x == 0 && isCyinder) {
+				lst.push_back(index - 1);	
+			}
+			// 6
+			else if (x == 0 && ! isCyinder) {
+				// pass
+			}
+			// 7 -> 3
+			else {
+				lst.push_back(index - width - 1);	
+			}
+
+			//// upper right ////
+
+			// 8 -> 5
+			lst.push_back(index - width);	
+		}
+		// y == 1 or 3
+		else {
+			//// upper left ////
+
+			// 3 -> 0
+			lst.push_back(index - width);
+
+
+			//// upper right ////
+
+			// 5 -> 0
+			if (x == width - 1 && isCyinder) {
+				lst.push_back(index - width * 2 - 1);	
+			}
+			// 5
+			else if (x == width - 1 && ! isCyinder) {
+				// pass
+			}
+			// 4 -> 3
+			else {
+				lst.push_back(index - width - 1);	
+			}
+		}
+	}
+	else if (y < height - 1) {
+	
+		// y == 0 or 2
+		if (y % 2 == 0) {
+
+			//// left lower ////
+
+			// 6 -> 11 
+			if (x == 0 && isCyinder) {
+				lst.push_back(index + width * 2 - 1);	
+			}
+			// 6
+			else if (x == 0 && ! isCyinder) {
+				// pass
+			}
+			// 7 -> 9
+			else {
+				lst.push_back(index + width - 1);	
+			}
+
+			//// right lower ////
+
+			// 8 -> 11
+			lst.push_back(index + width);	
+		}
+		else {
+			//// upper left ////
+
+			// 3 -> 6
+			lst.push_back(index + width);
+
+
+			//// upper right ////
+
+			// 5 -> 6
+			if (x == width - 1 && isCyinder) {
+				lst.push_back(index + 1);	
+			}
+			// 5
+			else if (x == width - 1 && ! isCyinder) {
+				// pass
+			}
+			// 4 -> 8
+			else {
+				lst.push_back(index + width + 1);	
+			}
+		}
+	}
+	
+	return lst;
+}
+
 std::list<int> makeListInRange (int middle, int addSub, int bottom, int upper)
 {
 	std::list<int> lst;
