5ef147fc65be3abc5da03144f0d6e50abd263e1d
reu <ryoiwanaga@gmail.com>
2016-03-22 14:26:11 +0900

    squareverのコードの削除

--- World.cpp
+++ World.cpp
@@ -1,24 +1,12 @@
 #include "World.h"
 #include "Hex.h"
 
-#include <algorithm>
 #include <cassert>
 #include <list>
 #include <set>
 #include <iterator>
 #include <random>
 
-#include <iostream>
-
-
-std::list<int> makeListInRange (int middle, int addSub, int bottom, int upper);
-//std::list<Pos> makeListCoastPositions(std::vector<Terrain>* terrains, int height, int width, bool isCylinder);
-
-
-int aat (int y, int x, int width) 
-{
-	return y * width + x;	
-}
 
 World::Ptr World::createMassive(int height, int width, bool isCylinder, float landPersentage)
 {
@@ -79,61 +67,5 @@ World::Ptr World::createMassive(int height, int width, bool isCylinder, float la
 	//					Terrain::FeatureType::None);
 
 	return world;
-}
-
-/*
-
-std::set<int> makeSetCoastPositions(std::vector<Terrain>* terrains, int height, int width, bool isCylinder)
-{
-	std::set<int> coasts;
-
-	// For each square
-	for (int y = 2; y < height - 2; y++) {
-		for (int x = 2; x < width - 2; x++) {
-
-			auto terrain = &(terrains->at(aat(y, x, width)));
-
-			// Skip If searching square is land
-			if (terrain->base == Terrain::BaseType::Glassland)
-				continue;
-
-			auto rangeY = makeListInRange(y, 1, 0, height - 1);
-			auto rangeX = makeListInRange(x, 1, 0, width - 1);
-
-			// For each neighbor
-			for (auto yy : rangeY) {
-				for (auto xx : rangeX) {
-
-					if (terrains->at(aat(yy, xx, width)).base == Terrain::BaseType::Glassland) {
-						// lst.push_back(terrain);
-						lst.push_back(Pos(x, y));
-
-						goto LABEL_FOUND_COAST;
-					}
-				}
-			}
-
-LABEL_FOUND_COAST:
-			assert(true);
-		}
-	}
-
-	return lst;
-}
-*/
-
-
-std::list<int> makeListInRange (int middle, int addSub, int bottom, int upper)
-{
-	std::list<int> lst;
-
-	auto min = std::max (middle - addSub, bottom);
-	auto max = std::min (middle + addSub, upper);
-
-	for (int n = min; n <= max; n++) {
-		lst.push_back(n);	
-	}
-
-	return lst;
 }
 
